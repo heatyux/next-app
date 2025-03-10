74e0c4b4ae3a50e66b35df0126ece30f3a60a66d
Lux <1171484646@qq.com>
2025-03-10 18:25:48 +0800

    fix: database queries

--- src/features/ticket/queries/get-ticket.ts
+++ src/features/ticket/queries/get-ticket.ts
@@ -1,21 +1,10 @@
-import { initialTickets } from "@/data"
+import { prisma } from '@/lib/prisma'
 
-import type { Ticket } from "../types"
+export const getTicket = async (id: string) => {
 
-export const getTickets = async (): Promise<Ticket[]> => {
-  await new Promise(resolve => setTimeout(resolve, 2000))
-
-  return new Promise(resolve => {
-    resolve(initialTickets)
-  })
-}
-
-export const getTicket = async (id: string): Promise<Ticket | null> => {
-  await new Promise(resolve => setTimeout(resolve, 2000))
-
-  const maybeTicket = initialTickets.find(ticket => ticket.id === id)
-
-  return new Promise(resolve => {
-    resolve(maybeTicket || null)
+  return prisma.ticket.findUnique({
+    where: {
+      id
+    }
   })
 }
