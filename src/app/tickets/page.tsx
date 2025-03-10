f89da3152655443c6fbc33e4808c8fd1ea1f7137
Lux <1171484646@qq.com>
2025-03-10 11:20:30 +0800

    fix: feature folder tickets

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,15 +1,6 @@
-import Link from "next/link"
 import { initialTickets } from "@/data"
-import { ticketPath } from "@/path"
-import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
-import { LucideCheckCircle, LucideFileText, LucidePencil } from "lucide-react"
 import Heading from "@/components/heading"
-
-const TICKET_ICONS = {
-  OPEN: <LucideFileText />,
-  IN_PROGRESS: <LucideCheckCircle />,
-  DONE: <LucidePencil />
-}
+import { TicketItem } from "@/features/ticket/components/ticket-item"
 
 const TicketsPage = () => {
   return (
@@ -18,24 +9,7 @@ const TicketsPage = () => {
       <div className="flex-1 flex flex-col items-center gap-y-4 animate-fade-in-from-top">
         {
           initialTickets.map(ticket => (
-            <Card key={ticket.id} className="w-full max-w-[420px]">
-              <CardHeader>
-                <CardTitle className="flex gap-x-2">
-                  <span>{TICKET_ICONS[ticket.status]}</span>
-                  <span className="truncate">{ticket.title}</span>
-                </CardTitle>
-              </CardHeader>
-              <CardContent>
-                <span className="line-clamp-3 whitespace-break-spaces">
-                  {ticket.content}
-                </span>
-              </CardContent>
-              <CardFooter>
-                <Link href={ticketPath(ticket.id)} className="underline">
-                  View
-                </Link>
-              </CardFooter>
-            </Card>
+            <TicketItem key={ticket.id} ticket={ticket} />
           ))
         }
       </div>
