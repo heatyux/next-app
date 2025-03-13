0515781733a05a94afc48ae8fbda9c4214757a65
Lux <1171484646@qq.com>
2025-03-13 11:02:15 +0800

    fix: form validation

--- src/features/ticket/actions/upsert-ticket.ts
+++ src/features/ticket/actions/upsert-ticket.ts
@@ -2,26 +2,27 @@
 
 import { revalidatePath } from 'next/cache'
 import { redirect } from 'next/navigation'
+import { z } from 'zod'
 import { prisma } from '@/lib/prisma'
 import { ticketPath, ticketsPath } from '@/path'
 
+const upsertTicketSchema = z.object({
+  title: z.string().min(1).max(191),
+  content: z.string().min(1).max(1024)
+})
+
 export const upsertTicket = async (id: string | undefined, formData: FormData) => {
-  const data = {
+  const data = upsertTicketSchema.parse({
     title: formData.get('title'),
     content: formData.get('content'),
-  }
-
-  const dbData = {
-    title: data.title as string,
-    content: data.content as string
-  }
+  })
 
   await prisma.ticket.upsert({
     where: {
       id: id || ''
     },
-    create: dbData,
-    update: dbData
+    create: data,
+    update: data
   })
 
   revalidatePath(ticketsPath())
