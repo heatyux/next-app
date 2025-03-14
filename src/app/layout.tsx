9eb91be77d70a960000e62ee5470eca81a6a4b44
Lux <1171484646@qq.com>
2025-03-14 18:24:01 +0800

    fix: component folder header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,7 @@
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
-import Link from 'next/link'
-import { homePath, ticketsPath } from '@/paths'
-import { buttonVariants } from '@/components/ui/button'
-import { LucideKanban } from 'lucide-react'
+import { Header } from '@/components/header'
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -31,25 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <nav className="supports-backdrop-blur:bg-background/60 bg-background/95 fixed top-0 right-0 left-0 z-20 flex w-full justify-between border-b px-5 py-2.5 backdrop-blur">
-          <div>
-            <Link
-              href={homePath()}
-              className={buttonVariants({ variant: 'ghost' })}
-            >
-              <LucideKanban />
-              <h1 className="text-lg font-semibold">TicketBounty</h1>
-            </Link>
-          </div>
-          <div>
-            <Link
-              href={ticketsPath()}
-              className={buttonVariants({ variant: 'default' })}
-            >
-              Tickets
-            </Link>
-          </div>
-        </nav>
+        <Header />
         <main className="bg-secondary/20 flex min-h-screen flex-1 flex-col overflow-x-hidden overflow-y-auto px-8 py-24">
           {children}
         </main>
---
+++ src/components/header.tsx
@@ -0,0 +1,30 @@
+import Link from 'next/link'
+import { homePath, ticketsPath } from '@/paths'
+import { buttonVariants } from '@/components/ui/button'
+import { LucideKanban } from 'lucide-react'
+
+const Header = () => {
+  return (
+    <nav className="supports-backdrop-blur:bg-background/60 bg-background/95 fixed top-0 right-0 left-0 z-20 flex w-full justify-between border-b px-5 py-2.5 backdrop-blur">
+      <div>
+        <Link
+          href={homePath()}
+          className={buttonVariants({ variant: 'ghost' })}
+        >
+          <LucideKanban />
+          <h1 className="text-lg font-semibold">TicketBounty</h1>
+        </Link>
+      </div>
+      <div>
+        <Link
+          href={ticketsPath()}
+          className={buttonVariants({ variant: 'default' })}
+        >
+          Tickets
+        </Link>
+      </div>
+    </nav>
+  )
+}
+
+export { Header }
