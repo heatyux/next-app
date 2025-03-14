8268c0b66c46561e5d0ff99a70be58f10ca4c4fb
Lux <1171484646@qq.com>
2025-03-14 17:58:26 +0800

    fix: ui library card

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -2,6 +2,13 @@ import Link from 'next/link'
 import { initialTickets } from '@/data'
 import { ticketPath } from '@/paths'
 import clsx from 'clsx'
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
 
 const CheckIcon = () => (
   <svg
@@ -71,23 +78,24 @@ const TicketsPage = () => {
       </div>
       <div className="animate-fade-from-top flex flex-1 flex-col items-center gap-y-4">
         {initialTickets.map((ticket) => (
-          <div
-            key={ticket.id}
-            className="w-full max-w-[420px] rounded border border-slate-100 p-4"
-          >
-            <div>{TICKET_ICONS[ticket.status]}</div>
-            <h3 className="truncate text-lg font-semibold">{ticket.title}</h3>
-            <p
-              className={clsx('truncate text-sm text-slate-500', {
-                'line-through': ticket.status === 'DONE',
-              })}
-            >
-              {ticket.content}
-            </p>
-            <Link href={ticketPath(ticket.id)} className="text-sm underline">
-              View
-            </Link>
-          </div>
+          <Card key={ticket.id} className="w-full max-w-[420px]">
+            <CardHeader>
+              <CardTitle className="flex gap-x-2">
+                <span>{TICKET_ICONS[ticket.status]}</span>
+                <span className="truncate">{ticket.title}</span>
+              </CardTitle>
+            </CardHeader>
+            <CardContent>
+              <span className="line-clamp-3 whitespace-break-spaces">
+                {ticket.content}
+              </span>
+            </CardContent>
+            <CardFooter>
+              <Link href={ticketPath(ticket.id)} className="text-sm underline">
+                View
+              </Link>
+            </CardFooter>
+          </Card>
         ))}
       </div>
     </div>
