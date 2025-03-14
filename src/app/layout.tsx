2ebf49d4c4d45f2a7cec948657429a8a1f5e08af
Lux <1171484646@qq.com>
2025-03-14 17:51:41 +0800

    fix: ui library button

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
 import Link from 'next/link'
 import { homePath, ticketsPath } from '@/paths'
+import { buttonVariants } from '@/components/ui/button'
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -31,12 +32,18 @@ export default function RootLayout({
       >
         <nav className="supports-backdrop-blur:bg-background/60 bg-background/95 fixed top-0 right-0 left-0 z-20 flex w-full justify-between border-b px-5 py-2.5 backdrop-blur">
           <div>
-            <Link href={homePath()} className="text-lg font-bold">
+            <Link
+              href={homePath()}
+              className={buttonVariants({ variant: 'outline' })}
+            >
               Home
             </Link>
           </div>
           <div>
-            <Link href={ticketsPath()} className="text-sm underline">
+            <Link
+              href={ticketsPath()}
+              className={buttonVariants({ variant: 'outline' })}
+            >
               Tickets
             </Link>
           </div>
