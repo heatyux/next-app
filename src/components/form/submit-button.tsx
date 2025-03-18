c044175a7edb9a090f57ab7efcf54c25e9c695ef
Lux <1171484646@qq.com>
2025-03-18 17:26:55 +0800

    fix: form use status

---
+++ src/components/form/submit-button.tsx
@@ -0,0 +1,20 @@
+import { useFormStatus } from 'react-dom'
+import { Button } from '@/components/ui/button'
+import { LucideLoaderCircle } from 'lucide-react'
+
+type SubmitButtonProps = {
+  label: string
+}
+
+const SubmitButton = ({ label }: SubmitButtonProps) => {
+  const { pending } = useFormStatus()
+
+  return (
+    <Button type="submit" disabled={pending}>
+      {pending && <LucideLoaderCircle className="h-4 w-4 animate-spin" />}
+      {label}
+    </Button>
+  )
+}
+
+export { SubmitButton }
--- src/features/ticket/components/ticket-upsert-form.tsx
+++ src/features/ticket/components/ticket-upsert-form.tsx
@@ -1,39 +1,29 @@
 'use client'
 
-import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { Textarea } from '@/components/ui/textarea'
 import { upsertTicket } from '../actions/upsert-ticket'
+import { SubmitButton } from '@/components/form/submit-button'
 import type { Ticket } from '@prisma/client'
-import { useTransition } from 'react'
-import { LucideLoaderCircle } from 'lucide-react'
 
 type TicketUpsertFormProps = {
   ticket?: Ticket
 }
 
 const TicketUpsertForm: React.FC<TicketUpsertFormProps> = ({ ticket }) => {
-  const [isPending, startTransition] = useTransition()
-
-  const upsertTicketAction = (formData: FormData) => {
-    startTransition(async () => {
-      await upsertTicket(ticket?.id, formData)
-    })
-  }
-
   return (
-    <form action={upsertTicketAction} className="flex flex-col gap-y-2">
+    <form
+      action={upsertTicket.bind(null, ticket?.id)}
+      className="flex flex-col gap-y-2"
+    >
       <Label htmlFor="title">Title</Label>
       <Input id="title" name="title" defaultValue={ticket?.title} />
 
       <Label htmlFor="content">Content</Label>
       <Textarea id="content" name="content" defaultValue={ticket?.content} />
 
-      <Button type="submit" disabled={isPending}>
-        {isPending && <LucideLoaderCircle className="h-4 w-4 animate-spin" />}
-        {ticket ? 'Edit' : 'Create'}
-      </Button>
+      <SubmitButton label={ticket ? 'Edit' : 'Create'} />
     </form>
   )
 }
