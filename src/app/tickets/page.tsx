e62a105e307ddc0261b6b485e75a45cf0a7a9350
Lux <1171484646@qq.com>
2025-03-18 15:26:16 +0800

    fix: server actions create form

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -2,11 +2,29 @@ import { Suspense } from 'react'
 import { Heading } from '@/components/heading'
 import { TicketList } from '@/features/ticket/components/ticket-list'
 import { Spinner } from '@/components/spinner'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+import { TicketCreateForm } from '@/features/ticket/components/ticket-create-form'
 
 const TicketsPage = async () => {
   return (
     <div className="flex flex-1 flex-col gap-y-8">
       <Heading title="Tickets" description="All your tickets at one place" />
+
+      <Card className="w-full max-w-[420px] self-center">
+        <CardHeader>
+          <CardTitle>Create Ticket</CardTitle>
+          <CardDescription>A new ticket will be created</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <TicketCreateForm />
+        </CardContent>
+      </Card>
 
       <Suspense fallback={<Spinner />}>
         <TicketList />
