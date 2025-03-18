4a90843dc7b915b40f2ff47ff570cff9af3bba1f
Lux <1171484646@qq.com>
2025-03-18 10:02:11 +0800

    fix: server components streaming

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,19 +1,15 @@
+import { Suspense } from 'react'
 import { Heading } from '@/components/heading'
-import { TicketItem } from '@/features/ticket/components/ticket-item'
-import { getTickets } from '@/features/ticket/queries/get-tickets'
+import { TicketList } from '@/features/ticket/components/ticket-list'
 
 const TicketsPage = async () => {
-  const tickets = await getTickets()
-
   return (
     <div className="flex flex-1 flex-col gap-y-8">
       <Heading title="Tickets" description="All your tickets at one place" />
 
-      <div className="animate-fade-from-top flex flex-1 flex-col items-center gap-y-4">
-        {tickets.map((ticket) => (
-          <TicketItem key={ticket.id} ticket={ticket} />
-        ))}
-      </div>
+      <Suspense>
+        <TicketList />
+      </Suspense>
     </div>
   )
 }
