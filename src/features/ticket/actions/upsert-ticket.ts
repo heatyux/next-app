5e29c7358a6aafdbda037f2b88863ea7e817ea58
Lux <1171484646@qq.com>
2025-03-18 17:43:00 +0800

    fix: form validate

--- src/features/ticket/actions/upsert-ticket.ts
+++ src/features/ticket/actions/upsert-ticket.ts
@@ -4,22 +4,32 @@ import { revalidatePath } from 'next/cache'
 import { redirect } from 'next/navigation'
 import { prisma } from '@/lib/prisma'
 import { ticketsPath } from '@/paths'
+import { z } from 'zod'
+
+const upsertTicketSchema = z.object({
+  title: z.string().min(1).max(191),
+  content: z.string().min(1).max(1024),
+})
 
 export const upsertTicket = async (
   id: string | undefined,
   _actionState: { message: string },
   formData: FormData,
 ) => {
-  const data = {
-    title: formData.get('title') as string,
-    content: formData.get('content') as string,
-  }
+  try {
+    const data = upsertTicketSchema.parse({
+      title: formData.get('title'),
+      content: formData.get('content'),
+    })
 
-  await prisma.ticket.upsert({
-    where: { id: id || '' },
-    create: data,
-    update: data,
-  })
+    await prisma.ticket.upsert({
+      where: { id: id || '' },
+      create: data,
+      update: data,
+    })
+  } catch {
+    return { message: 'Something went wrong' }
+  }
 
   revalidatePath(ticketsPath())
 
