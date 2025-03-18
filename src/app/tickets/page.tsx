56ec1e3ed1f15ffc208489a45b24821ae8f06f92
Lux <1171484646@qq.com>
2025-03-18 15:35:11 +0800

    fix: server actions form configuretion

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -2,29 +2,20 @@ import { Suspense } from 'react'
 import { Heading } from '@/components/heading'
 import { TicketList } from '@/features/ticket/components/ticket-list'
 import { Spinner } from '@/components/spinner'
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card'
 import { TicketCreateForm } from '@/features/ticket/components/ticket-create-form'
+import { CardCompact } from '@/components/card-compact'
 
 const TicketsPage = async () => {
   return (
     <div className="flex flex-1 flex-col gap-y-8">
       <Heading title="Tickets" description="All your tickets at one place" />
 
-      <Card className="w-full max-w-[420px] self-center">
-        <CardHeader>
-          <CardTitle>Create Ticket</CardTitle>
-          <CardDescription>A new ticket will be created</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <TicketCreateForm />
-        </CardContent>
-      </Card>
+      <CardCompact
+        className="w-full max-w-[420px] self-center"
+        title="Create Ticket"
+        description="A new ticket will be created"
+        content={<TicketCreateForm />}
+      />
 
       <Suspense fallback={<Spinner />}>
         <TicketList />
---
+++ src/components/card-compact.tsx
@@ -0,0 +1,39 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+
+type CardCompactProps = {
+  title: string
+  description: string
+  content: React.ReactNode
+  footer?: React.ReactNode
+  className?: string
+}
+
+const CardCompact: React.FC<CardCompactProps> = ({
+  title,
+  description,
+  className,
+  content,
+  footer,
+}) => {
+  return (
+    <Card className={className}>
+      <CardHeader>
+        <CardTitle>{title}</CardTitle>
+        <CardDescription>{description}</CardDescription>
+      </CardHeader>
+      <CardContent>{content}</CardContent>
+      {footer && (
+        <CardFooter className="flex justify-between">{footer}</CardFooter>
+      )}
+    </Card>
+  )
+}
+
+export { CardCompact }
