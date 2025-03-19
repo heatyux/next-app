c22231ebe3968a0e8817f83ebb82c61455ebf3b0
Lux <1171484646@qq.com>
2025-03-19 09:52:42 +0800

    fix: form reset

--- src/features/ticket/components/ticket-upsert-form.tsx
+++ src/features/ticket/components/ticket-upsert-form.tsx
@@ -21,10 +21,22 @@ const TicketUpsertForm: React.FC<TicketUpsertFormProps> = ({ ticket }) => {
   return (
     <form action={action} className="flex flex-col gap-y-2">
       <Label htmlFor="title">Title</Label>
-      <Input id="title" name="title" defaultValue={ticket?.title} />
+      <Input
+        id="title"
+        name="title"
+        defaultValue={
+          (actionState?.payload?.get('title') as string) ?? ticket?.title
+        }
+      />
 
       <Label htmlFor="content">Content</Label>
-      <Textarea id="content" name="content" defaultValue={ticket?.content} />
+      <Textarea
+        id="content"
+        name="content"
+        defaultValue={
+          (actionState?.payload?.get('content') as string) ?? ticket?.content
+        }
+      />
 
       <SubmitButton label={ticket ? 'Edit' : 'Create'} />
 
