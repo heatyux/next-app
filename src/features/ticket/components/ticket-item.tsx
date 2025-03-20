aaea57cda28a8f6eaf4aa6b61216bff1602b356c
Lux <1171484646@qq.com>
2025-03-20 18:08:00 +0800

    fix: ui library dropdown menu

--- src/features/ticket/components/ticket-item.tsx
+++ src/features/ticket/components/ticket-item.tsx
@@ -2,6 +2,7 @@ import type { Ticket } from '@prisma/client'
 import clsx from 'clsx'
 import {
   LucideArrowUpRightFromSquare,
+  LucideMoreVertical,
   LucidePencil,
   LucideTrash,
 } from 'lucide-react'
@@ -18,6 +19,7 @@ import { ticketEditPath, ticketPath } from '@/paths'
 import { toCurrencyFromCent } from '@/utils/currency'
 import { deleteTicket } from '../actions/delete-ticket'
 import { TICKET_ICONS } from '../constants'
+import { TicketMoreMenu } from './ticket-more-menu'
 
 type TicketProps = {
   ticket: Ticket
@@ -47,6 +49,16 @@ const TicketItem = ({ ticket, isDetail }: TicketProps) => {
         <LucideTrash className="h-4 w-4" />
       </Button>
     </form>
+  )
+
+  const moreMenu = (
+    <TicketMoreMenu
+      trigger={
+        <Button variant="outline" size="icon">
+          <LucideMoreVertical className="h-4 w-4" />
+        </Button>
+      }
+    />
   )
 
   return (
@@ -85,6 +97,7 @@ const TicketItem = ({ ticket, isDetail }: TicketProps) => {
           <>
             {editButton}
             {deleteButton}
+            {moreMenu}
           </>
         ) : (
           <>
