8ef340ebd41f78d5051a88988a85f90298ef9a34
Lux <1171484646@qq.com>
2025-03-20 14:36:57 +0800

    fix: database migrations schema changes

--- src/features/ticket/constants.tsx
+++ src/features/ticket/constants.tsx
@@ -1,7 +1,7 @@
 import { LucideCheckCircle, LucideFileText, LucidePencil } from 'lucide-react'
 
 export const TICKET_ICONS = {
-  OPEN: <LucideFileText />,
-  IN_PROGRESS: <LucideCheckCircle />,
-  DONE: <LucidePencil />,
+  OPEN: <LucideFileText className="h-4 w-4" />,
+  IN_PROGRESS: <LucideCheckCircle className="h-4 w-4" />,
+  DONE: <LucidePencil className="h-4 w-4" />,
 }
