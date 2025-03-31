a095022920153489f79da19d94460f2f3461f3e7
Lux <1171484646@qq.com>
2025-03-31 19:10:15 +0800

    fix: authorization protected apis

--- src/features/ticket/actions/delete-ticket.ts
+++ src/features/ticket/actions/delete-ticket.ts
@@ -3,12 +3,29 @@
 import { revalidatePath } from 'next/cache'
 import { redirect } from 'next/navigation'
 import { setCookieByKey } from '@/actions/cookies'
-import { formErrorToActionState } from '@/components/form/utils/to-action-state'
+import {
+  formErrorToActionState,
+  toActionState,
+} from '@/components/form/utils/to-action-state'
+import { getAuthOrRedirect } from '@/features/auth/queries/get-auth-or-redirect'
+import { isOwner } from '@/features/auth/utils/is-owner'
 import { prisma } from '@/lib/prisma'
 import { ticketsPath } from '@/paths'
 
 export const deleteTicket = async (ticketId: string) => {
+  const { user } = await getAuthOrRedirect()
+
   try {
+    const ticket = await prisma.ticket.findUnique({
+      where: {
+        id: ticketId,
+      },
+    })
+
+    if (!ticket || !isOwner(user, ticket)) {
+      return toActionState('ERROR', 'Not authorized')
+    }
+
     await prisma.ticket.delete({
       where: {
         id: ticketId,
--- src/features/ticket/actions/upsert-ticket.ts
+++ src/features/ticket/actions/upsert-ticket.ts
@@ -9,9 +9,10 @@ import {
   formErrorToActionState,
   toActionState,
 } from '@/components/form/utils/to-action-state'
-import { getAuth } from '@/features/auth/queries/get-auth'
+import { getAuthOrRedirect } from '@/features/auth/queries/get-auth-or-redirect'
+import { isOwner } from '@/features/auth/utils/is-owner'
 import { prisma } from '@/lib/prisma'
-import { signInPath, ticketPath, ticketsPath } from '@/paths'
+import { ticketPath, ticketsPath } from '@/paths'
 import { toCent } from '@/utils/currency'
 
 const upsertTicketSchema = z.object({
@@ -26,10 +27,18 @@ export const upsertTicket = async (
   _actionState: ActionState,
   formData: FormData,
 ) => {
-  const { user } = await getAuth()
+  const { user } = await getAuthOrRedirect()
 
-  if (!user) {
-    redirect(signInPath())
+  if (id) {
+    const ticket = await prisma.ticket.findUnique({
+      where: {
+        id,
+      },
+    })
+
+    if (!ticket || !isOwner(user, ticket)) {
+      return toActionState('ERROR', 'Not authorized')
+    }
   }
 
   try {
