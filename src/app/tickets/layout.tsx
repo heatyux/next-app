a1a9382540f1bd479e234bf0193152655c64ad18
Lux <1171484646@qq.com>
2025-03-31 18:17:10 +0800

    fix: authorization protected routes

---
+++ src/app/tickets/layout.tsx
@@ -0,0 +1,12 @@
+import { ReactNode } from 'react'
+import { getAuthOrRedirect } from '@/features/auth/queries/get-auth-or-redirect'
+
+export default async function AuthenticatedLayout({
+  children,
+}: Readonly<{
+  children: ReactNode
+}>) {
+  await getAuthOrRedirect()
+
+  return <>{children}</>
+}
