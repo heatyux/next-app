e815d724de58095560ac86997a9d7e9cf79ac40d
Lux <1171484646@qq.com>
2025-04-01 17:41:06 +0800

    fix: advanced navigation breadcrumbs

--- src/app/tickets/[ticketId]/edit/page.tsx
+++ src/app/tickets/[ticketId]/edit/page.tsx
@@ -1,9 +1,12 @@
 import { notFound } from 'next/navigation'
+import { Breadcrumbs } from '@/components/breadcrumbs'
 import { CardCompact } from '@/components/card-compact'
+import { Separator } from '@/components/ui/separator'
 import { getAuth } from '@/features/auth/queries/get-auth'
 import { isOwner } from '@/features/auth/utils/is-owner'
 import { TicketUpsertForm } from '@/features/ticket/components/ticket-upsert-form'
 import { getTicket } from '@/features/ticket/queries/get-ticket'
+import { homePath, ticketPath } from '@/paths'
 
 type TicketEditPageProps = {
   params: Promise<{ ticketId: string }>
@@ -22,13 +25,25 @@ export default async function TicketEditPage({ params }: TicketEditPageProps) {
   }
 
   return (
-    <div className="flex flex-1 flex-col items-center justify-center">
-      <CardCompact
-        className="animate-fade-from-top w-full max-w-[420px]"
-        title="Edit Ticket"
-        description="Edit an existing ticket"
-        content={<TicketUpsertForm ticket={ticket} />}
+    <div className="flex flex-1 flex-col gap-y-8">
+      <Breadcrumbs
+        breadcrumbs={[
+          { title: 'Tickets', href: homePath() },
+          { title: ticket.title, href: ticketPath(ticket.id) },
+          { title: 'Edit' },
+        ]}
       />
+
+      <Separator />
+
+      <div className="flex flex-1 flex-col items-center justify-center">
+        <CardCompact
+          className="animate-fade-from-top w-full max-w-[420px]"
+          title="Edit Ticket"
+          description="Edit an existing ticket"
+          content={<TicketUpsertForm ticket={ticket} />}
+        />
+      </div>
     </div>
   )
 }
---
+++ src/components/breadcrumbs.tsx
@@ -0,0 +1,48 @@
+import Link from 'next/link'
+import { Fragment } from 'react'
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from '@/components/ui/breadcrumb'
+
+type BreadcrumbsProps = {
+  breadcrumbs: {
+    title: string
+    href?: string
+  }[]
+}
+
+const Breadcrumbs = ({ breadcrumbs }: BreadcrumbsProps) => {
+  return (
+    <Breadcrumb>
+      <BreadcrumbList>
+        {breadcrumbs.map((breadcrumb, index) => {
+          let breadcrumbItem = (
+            <BreadcrumbPage>{breadcrumb.title}</BreadcrumbPage>
+          )
+
+          if (breadcrumb.href) {
+            breadcrumbItem = (
+              <BreadcrumbLink asChild>
+                <Link href={breadcrumb.href}>{breadcrumb.title}</Link>
+              </BreadcrumbLink>
+            )
+          }
+
+          return (
+            <Fragment key={breadcrumb.title}>
+              <BreadcrumbItem>{breadcrumbItem}</BreadcrumbItem>
+              {index < breadcrumbs.length - 1 && <BreadcrumbSeparator />}
+            </Fragment>
+          )
+        })}
+      </BreadcrumbList>
+    </Breadcrumb>
+  )
+}
+
+export { Breadcrumbs }
