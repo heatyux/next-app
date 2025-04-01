f3dddf5458b214c9bb5aaf23fde29bd243def6b0
Lux <1171484646@qq.com>
2025-04-01 12:04:25 +0800

    fix: advanced navigation all tickets page

--- src/features/ticket/components/ticket-list.tsx
+++ src/features/ticket/components/ticket-list.tsx
@@ -1,8 +1,12 @@
 import { TicketItem } from '@/features/ticket/components/ticket-item'
 import { getTickets } from '@/features/ticket/queries/get-tickets'
 
-const TicketList: React.FC = async () => {
-  const tickets = await getTickets()
+type TicketListProps = {
+  userId?: string
+}
+
+const TicketList: React.FC<TicketListProps> = async ({ userId }) => {
+  const tickets = await getTickets(userId)
 
   return (
     <div className="animate-fade-from-top flex flex-1 flex-col items-center gap-y-4">
