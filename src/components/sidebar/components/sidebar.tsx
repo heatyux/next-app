a2080aec1dfe61d3c05921819c766447caab5813
Lux <1171484646@qq.com>
2025-04-01 17:00:57 +0800

    fix: advanced navigation sidebar authenticated

--- src/components/sidebar/components/sidebar.tsx
+++ src/components/sidebar/components/sidebar.tsx
@@ -1,11 +1,14 @@
 'use client'
 
 import { useState } from 'react'
+import { useAuth } from '@/features/auth/hooks/use-auth'
 import { cn } from '@/lib/utils'
 import { navItems } from '../constants'
 import { SidebarItem } from './sidebar-item'
 
 const Sidebar = () => {
+  const { user, isFetched } = useAuth()
+
   const [isOpen, setIsOpen] = useState(false)
   const [transition, setTransition] = useState(false)
 
@@ -15,9 +18,14 @@ const Sidebar = () => {
     setTimeout(() => setTransition(false), 200)
   }
 
+  if (!user || !isFetched) {
+    return <div className="bg-secondary/20 w-[78px]"></div>
+  }
+
   return (
     <nav
       className={cn(
+        'animate-sidebar-from-left',
         'h-screen border-r pt-24',
         transition && 'duration-200',
         isOpen ? 'w-[78px] md:w-60' : 'w-[78px]',
