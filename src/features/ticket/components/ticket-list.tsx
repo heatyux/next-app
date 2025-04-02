6f3f31c2cc5db4b8aed8f798217b344108c60264
Lux <1171484646@qq.com>
2025-04-02 11:29:52 +0800

    fix: page search params

--- src/features/ticket/components/ticket-list.tsx
+++ src/features/ticket/components/ticket-list.tsx
@@ -1,13 +1,19 @@
+import { Placeholder } from '@/components/placeholder'
 import { SearchInput } from '@/components/search-input'
 import { TicketItem } from '@/features/ticket/components/ticket-item'
 import { getTickets } from '@/features/ticket/queries/get-tickets'
+import { SearchParams } from '../search-params'
 
 type TicketListProps = {
   userId?: string
+  searchParams: SearchParams
 }
 
-const TicketList: React.FC<TicketListProps> = async ({ userId }) => {
-  const tickets = await getTickets(userId)
+const TicketList: React.FC<TicketListProps> = async ({
+  userId,
+  searchParams,
+}) => {
+  const tickets = await getTickets(userId, searchParams)
 
   return (
     <div className="animate-fade-from-top flex flex-1 flex-col items-center gap-y-4">
@@ -15,9 +21,11 @@ const TicketList: React.FC<TicketListProps> = async ({ userId }) => {
         <SearchInput placeholder="Search tickets ..." />
       </div>
 
-      {tickets.map((ticket) => (
-        <TicketItem key={ticket.id} ticket={ticket} />
-      ))}
+      {tickets.length > 0 ? (
+        tickets.map((ticket) => <TicketItem key={ticket.id} ticket={ticket} />)
+      ) : (
+        <Placeholder label="Not tickets found" />
+      )}
     </div>
   )
 }
